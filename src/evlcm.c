5d0bff6d8fcbbda08dcf79ce3577681895845d21
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2022-08-05 20:03:51 -0700

    add timeout watcher and callback from ev manpage

--- src/evlcm.c
+++ src/evlcm.c
@@ -8,13 +8,27 @@
 
 #include <stdlib.h>
 
-int main( int argc, char ** argv ) {
-    args.verbosity = 0;
-    argp_parse( &argp, argc, argv, 0, 0, &args );
 
-    if(args.verbosity > 0) {
-        puts("Aloha!");
+static void timeout_cb (EV_P_ ev_timer *w, int revents) {
+    puts ("timeout");
+    // this causes the innermost ev_run to stop iterating
+    ev_break (EV_A_ EVBREAK_ONE);
+}
+
+
+int main (int argc, char ** argv) {
+    args.verbosity = 0;
+    argp_parse (&argp, argc, argv, 0, 0, &args);
+
+    if (args.verbosity > 0) {
+        puts ("Aloha!");
     }
 
-    exit( EXIT_SUCCESS );
+    struct ev_loop *loop = EV_DEFAULT;
+    ev_timer_init (&timeout_watcher, timeout_cb, 5.5, 0.);
+    ev_timer_start (loop, &timeout_watcher);
+
+    ev_run (loop, 0);
+
+    exit (EXIT_SUCCESS);
 }
--- src/evlcm.h
+++ src/evlcm.h
@@ -1,4 +1,10 @@
 #ifndef _EV_LCM_H_
 #define _EV_LCM_H_
 
+#include <ev.h>
+
+ev_timer timeout_watcher;
+
+static void timeout_cb (EV_P_ ev_timer *w, int revents);
+
 #endif // _EV_LCM_H_
