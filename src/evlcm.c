ffedc22d7df343c0be34566c3b462251938d8225
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2022-08-05 21:39:46 -0700

    add a way to quit
    TODO: Figure out how to read the character without a newline.

--- src/evlcm.c
+++ src/evlcm.c
@@ -8,6 +8,7 @@
 
 #include <inttypes.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 static inline int64_t ev_utime (struct ev_loop * loop) {
@@ -22,6 +23,20 @@ static void timeout_cb (EV_P_ ev_timer *w, int revents) {
 }
 
 
+static void stdin_cb (EV_P_ ev_io *w, int revents) {
+    if (args.verbosity > 2) {
+        fprintf (stderr, "%s:%d\n", __FILE__, __LINE__);
+    }
+    char c = (char)getchar();
+    if ('q' == c) {
+        ev_io_stop (EV_A_ w);
+        ev_break (EV_A_ EVBREAK_ALL);
+    } else {
+        fprintf (stderr, "unrecognized input: %c\n", c);
+    }
+}
+
+
 int main (int argc, char ** argv) {
     args.verbosity = 0;
     argp_parse (&argp, argc, argv, 0, 0, &args);
@@ -30,9 +45,16 @@ int main (int argc, char ** argv) {
         puts ("Aloha!");
     }
 
+
+    ev_timer timeout_watcher;
+    ev_io stdin_watcher;
+
     struct ev_loop *loop = EV_DEFAULT;
-    ev_timer_init (&timeout_watcher, timeout_cb, 0., 1e-1);
+    ev_timer_init (&timeout_watcher, timeout_cb, 0., 1.);
     ev_timer_again (loop, &timeout_watcher);
+
+    ev_io_init (&stdin_watcher, stdin_cb, STDIN_FILENO, EV_READ);
+    ev_io_start (loop, &stdin_watcher);
 
     ev_run (loop, 0);
 
--- src/evlcm.h
+++ src/evlcm.h
@@ -3,8 +3,6 @@
 
 #include <ev.h>
 
-ev_timer timeout_watcher;
-
 static void timeout_cb (EV_P_ ev_timer *w, int revents);
 
 #endif // _EV_LCM_H_
